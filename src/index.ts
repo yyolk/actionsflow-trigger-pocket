cf8862f429c638cda1775c9850d39a9917b9cf49
Joseph Chiocchi <input.output@gmail.com>
2023-11-28 07:13:50 +0000

    update comments

--- src/index.ts
+++ src/index.ts
@@ -27,12 +27,16 @@ export default class Pocket implements ITriggerClassType {
       throw new Error("Missing param consumerKey!");
     }
 
-    // timestamp in seconds - 90_000
+    // Calculate `now()` minus 25 hours as seconds.
+    // timestamp_in_seconds - 90_000
     // 90_000s = (25 h) as seconds = (60s * 60m * 25h)s
     const nowMinus25HoursTimestamp = Math.floor(Date.now() / 1000) - (90_000);
     const response = await new (
-      // This is necessary for the dependency call tree expecting CommonJS and this library being ESM.
       // I could've probably just did the HTTP call without figuring this out, and saved much more time.
+      // It's a learning experience, I think the next trigger I write, will contain all necessary logic.
+      // Especially if it's ultimately just a HTTP request and forming data.
+      //
+      // This is necessary for the dependency call tree expecting CommonJS and this library being ESM.
       await import("pocket-sdk-typescript/dist/lib/sdk.js").then(({default: PocketSDK}) => PocketSDK)
       )(consumerKey)
       .getItems(authToken, {
@@ -45,9 +49,7 @@ export default class Pocket implements ITriggerClassType {
     const items: AnyObject[] = Object.keys(response.list).map(key => {
         return { 
           ...response.list[key],
-          // convenience to work with tags as an array of strings
-          // and make sure it exists as a property
-          // tags: response.list[key].tags? Object.keys(response.list[key].tags) : [],
+          // Convenience to work with tags as an array of strings and make sure it exists as a property.
           tags: Object.keys(response.list[key].tags ?? {}),
         }
     });
